3a523318a2105078ed671fc439e82f4031296f33
Dipshirsha <140639876+Dipshirsha@users.noreply.github.com>
2023-09-29 22:29:26 +0530

    Update app.js

--- app.js
+++ app.js
@@ -21,7 +21,7 @@ const io = new Server(server);
 
 
 const host = process.env.HOST || 'localhost';
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 5000;
 
 const currentpath = path.resolve();
 console.log(currentpath);
