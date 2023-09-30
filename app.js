ba5bb9d90eef9070ec8e63ef7bdf4a1a43c1b00a
Dipshirsha <140639876+Dipshirsha@users.noreply.github.com>
2023-09-30 10:15:32 +0530

    Update app.js

--- app.js
+++ app.js
@@ -20,7 +20,7 @@ const server = http.createServer(app);
 const io = new Server(server);
 
 
-/* const host = process.env.HOST || 3000; */
+const host = process.env.HOST || 'localhost';
 const port = process.env.PORT || 5000;
 
 const currentpath = path.resolve();
@@ -698,6 +698,6 @@ io.on("connection", function (socket)  {
 app.listen(process.env.PORT || port, () => {
     console.log("server is working");
 })
-server.listen(port, '0.0.0.0', () => {
+server.listen(port, host, () => {
     console.log("Socket.IO server running at http://${host}:${port}/");
   });
